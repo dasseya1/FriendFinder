097d3ff61d069514ee230161291ea89cf2434229
Damien Asseya <dasseya1@gmail.com>
2017-05-12 01:41:25 +0000

    Updated api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,32 +29,48 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.post("/survey", function(req, res) {
-    // Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
 
-      friendData.push(req.body);
-      res.json(true);
+// Capture the user input object
+		var userInput = req.body;
+		// console.log('userInput = ' + JSON.stringify(userInput));
+
+		var userResponses = userInput.scores;
+		// console.log('userResponses = ' + userResponses);
+
+		// Compute best friend match
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000; // Make the initial value big for comparison
+
+		// Examine all existing friends in the list
+		for (var i = 0; i < friends.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				// console.log('Closest match found = ' + diff);
+				// console.log('Friend name = ' + friends[i].name);
+				// console.log('Friend image = ' + friends[i].photo);
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 
   });
 
-  // ---------------------------------------------------------------------------
-
-  app.get("/", function(req, res) {
-    res.json(friendData);
-  });
-
-  // API POST Requests
-  // Below code handles when a user clicks on submit and the form sends data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // ---------------------------------------------------------------------------
-
-  // ---------------------------------------------------------------------------
-
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data if needed - for testing purpose
-    friendData = [];
-
-  });
-};
+}
